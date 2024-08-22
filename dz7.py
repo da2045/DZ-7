d5ff23a7c1a5bbb61d9a7110221023fa38a7c02f
da2045 <vitde2009@gmail.com>
2024-08-22 23:41:08 +0300

    Add files via upload

---
+++ dz7.py
@@ -0,0 +1,81 @@
+# numerator = float(input("Введіть чисельник --> "))
+# denominator = float(input("Введіть знаменник --> "))
+# if denominator == 0:
+#     print("Помилка: ділення на нуль неможливе.")
+# else:
+#     result = numerator / denominator
+#     print(f"Результат ділення: {result}")
+
+
+
+
+
+# def divide_numbers(numerator, denominator):
+#     return numerator / denominator
+
+# numerator = float(input("Введіть чисельник --> "))
+# denominator = float(input("Введіть знаменник --Ю "))
+
+# if denominator == 0:
+#     print("Помилка: ділення на нуль неможливе.")
+# else:
+#     result = divide_numbers(numerator, denominator)
+#     print(f"Результат ділення: {result}")
+
+
+
+# def divide_numbers_safe(numerator, denominator):
+#     try:
+#         return numerator / denominator
+#     except ZeroDivisionError:
+#         return "Помилка: ділення на нуль неможливе."
+
+# numerator = float(input("Введіть чисельник --> "))
+# denominator = float(input("Введіть знаменник -->"))
+
+# result = divide_numbers_safe(numerator, denominator)
+# print(f"Результат ділення: {result}")
+
+
+
+
+
+
+# def convert_to_number(string):
+#     if string.replace('.', '', 1).isdigit():
+#         number = float(string)
+#         print(f"Перетворене число: {number}")
+#     else:
+#         print("Помилка: неможливо перетворити рядок на число.")
+
+# input_string = input("Введіть рядок --> ")
+# convert_to_number(input_string)
+
+
+
+
+
+
+
+# def convert_to_number(string):
+#     return float(string)
+
+# input_string = input("Введіть рядок--> ")
+# if input_string.replace('.', '', 1).isdigit():
+#     number = convert_to_number(input_string)
+#     print(f"Перетворене число--> {number}")
+# else:
+#     print("Помилка: неможливо перетворити рядок на число.")
+
+
+
+
+# def convert_to_number(string):
+#     if string.replace('.', '', 1).isdigit():
+#         number = float(string)
+#         print(f"Перетворене число --> {number}")
+#     else:
+#         print("Помилка: неможливо перетворити рядок на число.")
+
+# input_string = input("Введіть рядок -->")
+# convert_to_number(input_string)
